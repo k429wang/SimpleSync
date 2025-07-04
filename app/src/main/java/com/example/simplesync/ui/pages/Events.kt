417270f3bbb04e2a1576d6a10e8344a83b4cf26a
sarahgahyun <sarahgahyan@gmail.com>
2025-07-04 01:40:30 -0400

    add bottom nav bar scaffold

--- app/src/main/java/com/example/simplesync/ui/pages/Events.kt
+++ app/src/main/java/com/example/simplesync/ui/pages/Events.kt
@@ -1,7 +1,15 @@
 package com.example.simplesync.ui.pages
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.simplesync.ui.components.BottomNavBar
+import com.example.simplesync.ui.components.ScreenTitle
 import com.example.simplesync.ui.navigation.SimpleSyncNavController
 
 @Composable
@@ -9,4 +17,18 @@ fun EventPage(navController: SimpleSyncNavController) {
     Text(
         text = "Event Page"
     )
+
+    Scaffold(
+        bottomBar = { BottomNavBar(navController) }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(horizontal = 16.dp, vertical = 12.dp)
+        ) {
+            // Title
+            ScreenTitle("My events")
+        }
+    }
 }
