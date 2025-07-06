fb4118d13985c0d7898ae5766a468acfc964c25c
Kai Wang <k429wang@uwaterloo.ca>
2025-07-06 14:54:59 -0400

    connect friends page to db

---
+++ app/src/main/java/com/example/simplesync/model/Friend.kt
@@ -0,0 +1,16 @@
+package com.example.simplesync.model
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import java.util.UUID
+
+@Serializable
+data class Friend(
+    @SerialName("user_id") val userId: String = UUID.randomUUID().toString(), // User ID
+    @SerialName("friend_id") val friendId: String = UUID.randomUUID().toString(), // User ID
+    @SerialName("status") val status: Status,
+    @Contextual @SerialName("created_at") val createdAt: Instant = Clock.System.now(),
+)
