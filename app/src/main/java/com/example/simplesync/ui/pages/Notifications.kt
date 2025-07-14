454ff6e90cc30f78f0014f9ab69db8151617baac
sarahgahyun <sarahgahyan@gmail.com>
2025-07-14 16:43:14 -0400

    notifications ui title

--- app/src/main/java/com/example/simplesync/ui/pages/Notifications.kt
+++ app/src/main/java/com/example/simplesync/ui/pages/Notifications.kt
@@ -1,12 +1,39 @@
 package com.example.simplesync.ui.pages
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.simplesync.ui.components.BottomNavBar
+import com.example.simplesync.ui.components.ScreenTitle
 import com.example.simplesync.ui.navigation.SimpleSyncNavController
 
 @Composable
-fun NotificationsPage(navController: SimpleSyncNavController) {
-    Text(
-        text = "Notifications Page"
-    )
+@Preview
+fun NotificationsPage(navController: SimpleSyncNavController? = null) {
+    Scaffold(
+        bottomBar = {
+            if (navController != null) {
+                BottomNavBar(navController)
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(horizontal = 16.dp, vertical = 12.dp)
+        ) {
+            ScreenTitle("Notifications")
+            Spacer(modifier = Modifier.height(16.dp))
+
+        }
+    }
 }
+
